014da78432cf5c371860c7cc2cef2580c40caf98
Choi Eun Jeong <qhdrn67@naver.com>
2021-08-19 15:58:00 +0900

    Shared Components

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { darkModeVar, isLoggedInVar } from "./apollo";
 import { ThemeProvider } from "styled-components";
 import { darkTheme, GlobalStyles, lightTheme } from "./styles";
 import SignUp from "./screens/SignUp";
+import routes from "./routes";
 
 const App = () => {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
@@ -17,11 +18,11 @@ const App = () => {
       <GlobalStyles />
       <Router>
         <Switch>
-          <Route path="/" exact>
+          <Route path={routes.home} exact>
             {isLoggedIn ? <Home /> : <Login />}
           </Route>
           {!isLoggedIn ? (
-            <Route path="/sign-up">
+            <Route path={routes.signUp}>
               <SignUp />
             </Route>
           ) : null}
