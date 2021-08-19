3f1c5b5fc7160f1aece75ee2658ad29e0d9319e5
Choi Eun Jeong <qhdrn67@naver.com>
2021-08-19 16:35:30 +0900

    Sign Up UI

--- src/components/auth/Button.js
+++ src/components/auth/Button.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-const SButton = styled.input`
+const Button = styled.input`
   border: none;
   border-radius: 3px;
   margin-top: 12px;
@@ -11,8 +11,4 @@ const SButton = styled.input`
   font-weight: 600;
   width: 100%;
 `;
-
-function Button(props) {
-  return <SButton {...props} />;
-}
 export default Button;
