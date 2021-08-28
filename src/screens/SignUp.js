662315e56d1228fee4324a94ed6533e5f1bb73bd
Choi Eun Jeong <qhdrn67@naver.com>
2021-08-28 16:59:37 +0900

    Sign up

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,5 +1,6 @@
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useForm } from "react-hook-form";
 import styled from "styled-components";
 import AuthLayout from "../components/auth/AuthLayout";
 import BottomBox from "../components/auth/BottomBox";
@@ -9,6 +10,9 @@ import Input from "../components/auth/Input";
 import PageTitle from "../components/PageTitle";
 import { FatLink } from "../components/shared";
 import routes from "../routes";
+import { gql, useMutation } from "@apollo/client";
+import { useHistory } from "react-router-dom";
+import FormError from "../components/auth/FormError";
 
 const HeaderContainer = styled.div`
   display: flex;
@@ -22,7 +26,63 @@ const Subtitle = styled(FatLink)`
   margin-top: 10px;
 `;
 
+const CREATE_ACCOUNT_MATATION = gql`
+  mutation createAccount(
+    $firstName: String!
+    $lastName: String
+    $username: String!
+    $email: String!
+    $password: String!
+  ) {
+    createAccount(
+      firstName: $firstName
+      lastName: $lastName
+      username: $username
+      email: $email
+      password: $password
+    ) {
+      ok
+      error
+    }
+  }
+`;
+
 function SignUp() {
+  const { register, handleSubmit, setError, clearErrors, formState } = useForm({
+    mode: "onChange",
+  });
+
+  const history = useHistory();
+
+  const onCompleted = (data) => {
+    const {
+      createAccount: { ok, error },
+    } = data;
+    if (!ok) {
+      return setError("result", {
+        message: error,
+      });
+    }
+    history.push(routes.home);
+  };
+
+  const [createAccount, { loading }] = useMutation(CREATE_ACCOUNT_MATATION, {
+    onCompleted,
+  });
+
+  const onSubmitValid = (data) => {
+    if (loading) {
+      return;
+    }
+    createAccount({
+      variables: {
+        ...data,
+      },
+    });
+  };
+
+  const clearLoginError = () => clearErrors("result");
+
   return (
     <AuthLayout>
       <PageTitle title="Sign up" />
@@ -33,12 +93,72 @@ function SignUp() {
             Sign up to see photos and videos from your friends.
           </Subtitle>
         </HeaderContainer>
-        <form>
-          <Input type="text" placeholder="Name" />
-          <Input type="text" placeholder="Email" />
-          <Input type="text" placeholder="Username" />
-          <Input type="password" placeholder="Password" />
-          <Button type="submit" value="Sign up" />
+        <form onSubmit={handleSubmit(onSubmitValid)}>
+          <Input
+            {...register("username", {
+              required: "First Name is required",
+            })}
+            name="firstName"
+            type="text"
+            placeholder="First Name"
+            hasError={Boolean(formState.errors?.firstName?.message)}
+            onFocus={clearLoginError}
+          />
+          <FormError message={formState.errors?.firstName?.message} />
+          <Input
+            {...register("username", {
+              required: "Last Name is required",
+            })}
+            type="text"
+            placeholder="Last Name"
+            name="lastName"
+            hasError={Boolean(formState.errors?.lastName?.message)}
+            onFocus={clearLoginError}
+          />
+          <FormError message={formState.errors?.lastName?.message} />
+          <Input
+            {...register("username", {
+              required: "Email is required",
+            })}
+            name="email"
+            type="text"
+            placeholder="Email"
+            hasError={Boolean(formState.errors?.email?.message)}
+            onFocus={clearLoginError}
+          />
+          <FormError message={formState.errors?.email?.message} />
+          <Input
+            {...register("username", {
+              required: "Username is required",
+              minLength: {
+                value: 3,
+                message: "Username should be longer than 3 chars.",
+              },
+            })}
+            name="username"
+            type="text"
+            placeholder="Username"
+            hasError={Boolean(formState.errors?.username?.message)}
+            onFocus={clearLoginError}
+          />
+          <FormError message={formState.errors?.username?.message} />
+          <Input
+            {...register("username", {
+              required: "Password is required",
+            })}
+            name="password"
+            type="password"
+            placeholder="Password"
+            hasError={Boolean(formState.errors?.password?.message)}
+            onFocus={clearLoginError}
+          />
+          <FormError message={formState.errors?.password?.message} />
+          <Button
+            type="submit"
+            value={loading ? "Loading..." : "Sign up"}
+            disabled={!formState.isValid || loading}
+          />
+          <FormError message={formState.errors?.result?.message} />
         </form>
       </FormBox>
       <BottomBox cta="Have an account?" linkText="Log in" link={routes.home} />
