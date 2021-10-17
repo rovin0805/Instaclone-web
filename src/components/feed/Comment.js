28b053f8ac6feaf91089128306f1b5f592657bdd
Choi Eun Jeong <qhdrn67@naver.com>
2021-10-17 15:53:18 +0900

    parsing hash tag using sanitize-html

--- src/components/feed/Comment.js
+++ src/components/feed/Comment.js
@@ -1,17 +1,37 @@
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import { FatText } from "../shared";
+import sanitizeHtml from "sanitize-html";
 
 const CommentContainer = styled.div``;
 const CommentCaption = styled.span`
   margin-left: 10px;
+  mark {
+    background-color: inherit;
+    color: ${(props) => props.theme.accent};
+    cursor: pointer;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 `;
 
 const Comment = ({ author, payload }) => {
+  const cleanedPayload = sanitizeHtml(
+    payload.replace(/#[\w]+/g, "<mark>$&</mark>"),
+    {
+      allowedTags: ["mark"],
+    }
+  );
+
   return (
     <CommentContainer>
       <FatText>{author}</FatText>
-      <CommentCaption>{payload}</CommentCaption>
+      <CommentCaption
+        dangerouslySetInnerHTML={{
+          __html: cleanedPayload,
+        }}
+      />
     </CommentContainer>
   );
 };
